f4669f98d55a6f8345c02a54967abae17b76513a
Yasmany Hernandez <63424718+yasmoicc@users.noreply.github.com>
2022-08-01 18:25:15 -0500

    Aplicando una estructura real

--- models/Note.js
+++ models/Note.js
@@ -1,7 +1,14 @@
 const mongoose = require('mongoose')
 const noteShcema = new mongoose.Schema({
-  content: String,
-  date: Date,
+  content: {
+    type: String,
+    minlength: 5,
+    required: true
+  },
+  date: { 
+    type: Date,
+    required: true
+  },
   important: Boolean
 })
 noteShcema.set('toJSON', {
